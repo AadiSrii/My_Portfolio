8d49d42b5f6d844d3175aa17e1416db4a3f5dc3c
bernard powers <adarshsrivastava0348@gmail.com>
2024-05-07 23:21:28 +0530

    building phase

---
+++ script.js
@@ -0,0 +1,49 @@
+// typing script
+
+document.addEventListener("DOMContentLoaded", function() {
+    const typingText = document.getElementById("typing-text");
+    const originalText = typingText.innerText.split("");
+    
+    function typeEffect() {
+        typingText.innerText = "";
+        let i = 0;
+        const typeInterval = setInterval(function() {
+            if (i < originalText.length) {
+                typingText.innerText += originalText[i];
+                if (originalText[i] === " ") {
+                    typingText.innerText += "\u00A0";
+                }
+                i++;
+            } else {
+                clearInterval(typeInterval);
+                setTimeout(function() {
+                    typeEffect();
+                }, 1000);
+            }
+        }, 100);
+    }
+    
+    typeEffect();
+});
+
+
+
+// typng script end here
+
+
+var tablinks=document.getElementsByClassName("tab-links");
+var tabcontents=document.getElementsByClassName("tab-containts");
+
+function opentab(tabname){
+    for(let tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(let tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add( "active-link" );
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+// about section is closing here
