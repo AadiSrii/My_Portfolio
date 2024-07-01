066538e8afd73d8d0ae46018f0ab3911acdb29b5
AadiSrii <adarshsrivastava0348@gmail.com>
2024-07-01 14:48:32 +0530

    done

--- script.js
+++ script.js
@@ -61,3 +61,22 @@ function closemenu(){
     sidemen.style.right = "-200px";
     
 }
+
+
+// contact form
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbw1-9dWo7hqFwonGwN7VOnF_QlfRxQ1g7u7YB5Ce1STjtdsnq8SDEAh9lqrzg9iCycTdw/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg");
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = "";
+      },5000)
+      form.reset();
+    })
+      .catch(error => console.error('Error!', error.message))
+  })
